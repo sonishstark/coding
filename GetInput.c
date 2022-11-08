6e40fd8ecb66f643054798c7d1f518d1ac322283
sonishstark <sonishstark@gmail.com>
2022-11-08 20:39:20 +0530

    userInput

---
+++ GetInput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+    int b;
+    printf("a = ");
+    scanf("%d", &a );
+    printf("b = ");
+    scanf("%d", &b );
+    printf("sum = %d", (a+b));
+    return 0;
+} 
